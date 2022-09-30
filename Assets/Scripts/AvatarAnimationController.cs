44cc01aeaf059e413a9f0d9139189f6257bc6a17
Alireza <abahrema@asu.edu>
2022-09-30 11:51:12 -0700

    feet tracking
    - could use more work and improvements for walking animation speed, movement detection, and offsets.

---
+++ Assets/Scripts/AvatarAnimationController.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AvatarAnimationController : MonoBehaviour {
+    [SerializeField] private InputActionReference move;
+
+    [SerializeField] private Animator animator;    
+
+    public float mvmtThreshold;
+    private Rigidbody rb;
+    private Vector3 pastPosition, currentPosition;
+    private void Start() {
+        //rb = GetComponent<Rigidbody>();
+        if (mvmtThreshold == 0.0f) mvmtThreshold = 0.02f;
+    }
+
+    private void AnimateLegs6DOFMotion() {
+        this.animator.SetBool("isMoving", true);
+        this.animator.SetFloat("animSpeed", 2.0f);
+    }
+
+    private void StopAnimation6DOFMotion() {
+        this.animator.SetBool("isMoving", false);
+        this.animator.SetFloat("animSpeed", 0.0f);
+    }
+
+    private void Update() {
+        currentPosition = gameObject.transform.position;
+        float howFar = currentPosition.FlatDistanceTo(pastPosition);
+        Debug.Log($"Far {howFar}");
+
+        diff = Vector3.Distance(currentPosition, pastPosition);// Mathf.Abs(currentPosition.z - pastPosition.z); //Vector3.Distance(currentPosition, pastPosition);
+        if (diff > mvmtThreshold) {
+            AnimateLegs6DOFMotion();
+        } else {
+            StopAnimation6DOFMotion();
+        }
+        Debug.Log($"Diff {diff}");
+    }
+
+    public void SavePastPosition() {
+        pastPosition = gameObject.transform.position;
+    }
+
+    [SerializeField]
+    public float diff;
+    private void LateUpdate() {
+        
+        // check diff, 
+        //float zVel = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).z;
+        pastPosition = gameObject.transform.position;
+    }
+    private void OnEnable() {
+        if (move == null) return;
+        this.move.action.started += this.AnimateLegs;
+        this.move.action.canceled += this.StopAnimation;
+    }
+
+    private void OnDisable() {
+        if (move == null) return;
+        this.move.action.started -= this.AnimateLegs;
+        this.move.action.canceled -= this.StopAnimation;
+    }
+
+    private void AnimateLegs(InputAction.CallbackContext obj) {
+        bool isWalkingFoward = this.move.action.ReadValue<Vector2>().y > 0;
+
+        if (isWalkingFoward) {
+            this.animator.SetBool("isMoving", true);
+            this.animator.SetFloat("animSpeed", 1.0f);
+        } else {
+            this.animator.SetBool("isMoving", true);
+            this.animator.SetFloat("animSpeed", -1.0f);
+        }
+    }
+    private void StopAnimation(InputAction.CallbackContext obj) {
+        this.animator.SetBool("isMoving", false);
+        this.animator.SetFloat("animSpeed", 0.0f);
+    }
+}
+
+public static class Extns {
+    public static Vector2 xz(this Vector3 vv) {
+        return new Vector2(vv.x, vv.z);
+    }
+
+    public static float FlatDistanceTo(this Vector3 from, Vector3 unto) {
+        Vector2 a = from.xz();
+        Vector2 b = unto.xz();
+        return Vector2.Distance(a, b);
+    }
+}
---
+++ Assets/Scripts/AvatarFootController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class AvatarFootController : MonoBehaviour {
+    [SerializeField] private Animator animator;
+
+    [SerializeField] [Range(0, 1)] private float leftFootPosWeight;
+    [SerializeField] [Range(0, 1)] private float rightFootPosWeight;
+
+    [SerializeField] [Range(0, 1)] private float leftFootRotWeight;
+    [SerializeField] [Range(0, 1)] private float rightFootRotWeight;
+
+    [SerializeField] private Vector3 footOffset;
+    [SerializeField] private Vector3 raycastOffsetLeft;
+    [SerializeField] private Vector3 raycastOffsRight;
+
+    private void OnAnimatorIK(int layerIndex) {
+        Vector3 leftFootPos = this.animator.GetIKPosition(AvatarIKGoal.LeftFoot);
+        Vector3 rightFootPos = this.animator.GetIKPosition(AvatarIKGoal.RightFoot);
+
+        RaycastHit hitLeftFoot;
+        RaycastHit hitRightFoot;
+
+        bool isLeftFootDown = Physics.Raycast(leftFootPos + this.raycastOffsetLeft, Vector3.down, out hitLeftFoot);
+        bool isRightFootDown = Physics.Raycast(rightFootPos + this.raycastOffsRight, Vector3.down, out hitRightFoot);
+
+        if (isLeftFootDown) {
+            this.animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, this.leftFootPosWeight);
+            this.animator.SetIKPosition(AvatarIKGoal.LeftFoot, hitLeftFoot.point + this.footOffset);
+
+            Quaternion leftFootRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hitLeftFoot.normal), hitLeftFoot.normal);
+            this.animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, this.leftFootRotWeight);
+            this.animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootRotation);
+        } else {
+            this.animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 0);
+        }
+
+        if (isRightFootDown) {
+            this.animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, this.rightFootPosWeight);
+            this.animator.SetIKPosition(AvatarIKGoal.RightFoot, hitRightFoot.point + this.footOffset);
+
+            Quaternion rightFootRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hitRightFoot.normal), hitRightFoot.normal);
+            this.animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, this.rightFootRotWeight);
+            this.animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootRotation);
+        } else {
+            this.animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 0);
+        }
+    }
+}
