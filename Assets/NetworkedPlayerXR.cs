ab7db5113c0cdc64ee38f0c87de7a0a7150c3793
Alireza <abahrema@asu.edu>
2022-10-05 23:48:45 -0700

    Updated offsets on hands

--- Assets/NetworkedPlayerXR.cs
+++ Assets/NetworkedPlayerXR.cs
@@ -13,7 +13,7 @@ public class NetworkedPlayerXR : NetworkBehaviour
         if (IsOwner) {
             SendTargetToServerRPC(transform.position);
         }
-        if (IsHost || IsServer || IsClient) {
+        if (IsHost || IsServer) {
             transform.position = m_TargetServerOnly;
         }
     }
